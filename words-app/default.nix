fe76ea05ca0cfe2602b32b187ca5f37d5412d2de
Petter Rasmussen <petter.rasmussen@gmail.com>
2018-12-20 13:31:57 +0100

    [words-app] Use hpack

--- words-app/default.nix
+++ words-app/default.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, aeson, base, bytestring, lucid, random-fu, safe
-, servant-lucid, servant-server, stdenv, text, warp
+{ mkDerivation, aeson, base, bytestring, hpack, lucid, random-fu
+, safe, servant-lucid, servant-server, stdenv, text, warp
 }:
 mkDerivation {
   pname = "words-app";
@@ -11,6 +11,7 @@ mkDerivation {
     aeson base bytestring lucid random-fu safe servant-lucid
     servant-server text warp
   ];
+  libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
     aeson base bytestring lucid random-fu safe servant-lucid
     servant-server text warp
@@ -19,6 +20,7 @@ mkDerivation {
     aeson base bytestring lucid random-fu safe servant-lucid
     servant-server text warp
   ];
+  preConfigure = "hpack";
   homepage = "https://github.com/prasmussen/words-app#readme";
   license = stdenv.lib.licenses.bsd3;
 }
