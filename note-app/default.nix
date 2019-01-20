6c8a3f6394e6169cf24ac6fa4436f850d67c715e
Petter Rasmussen <petter.rasmussen@gmail.com>
2019-01-20 20:30:58 +0100

    [note-app] Add nix build script

---
+++ note-app/default.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  src =
+    ./.;
+
+  cmd =
+    ''
+    appJsHash="$(sha1sum ${src}/app.min.js | awk '{print $1}')"
+
+    mkdir -p $out
+    cat ${src}/index.html | sed -e "s/app.js/app.js?hash=$appJsHash/" > $out/index.html
+    cp ${src}/app.min.js $out/app.js
+    '';
+in
+pkgs.runCommand "note-app" {} cmd
