2d58ecac6b89ca7e3dfeba795686814f213dd95d
Petter Rasmussen <petter.rasmussen@gmail.com>
2019-01-26 14:19:23 +0100

    [reminders-app] Add input fields

--- reminders-app/src/Main.elm
+++ reminders-app/src/Main.elm
@@ -3,6 +3,12 @@ module Main exposing (main)
 import Browser
 import Browser.Extra as BrowserExtra
 import Browser.Navigation as Nav
+import Element exposing (Element)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
+import Element.Input as Input
 import Html exposing (Html)
 import Html.Attributes as Attrs
 import Html.Events as Events
@@ -27,12 +33,16 @@ main =
 
 type alias Model =
     { time : Time.Posix
+    , title : String
+    , when : String
     }
 
 
 type Msg
     = ClickedLink Browser.UrlRequest
     | UrlChange Url.Url
+    | SetTitle String
+    | SetWhen String
 
 
 init : () -> Url.Url -> Nav.Key -> ( Task Never Model, Cmd Msg )
@@ -43,6 +53,8 @@ init flags url navKey =
 
         toModel time =
             { time = time
+            , title = ""
+            , when = ""
             }
     in
     ( task, Cmd.none )
@@ -62,21 +74,100 @@ update msg model =
         UrlChange url ->
             ( model, Cmd.none )
 
+        SetTitle title ->
+            ( { model | title = title }, Cmd.none )
 
-title : String
-title =
+        SetWhen when ->
+            let
+                _ =
+                    Moment.parse when
+                        |> Debug.log "moment"
+            in
+            ( { model | when = when }, Cmd.none )
+
+
+pageTitle : String
+pageTitle =
     "Reminders"
 
 
 loadingView : Browser.Document Msg
 loadingView =
-    { title = title
+    { title = pageTitle
     , body = []
     }
 
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = title
-    , body = [ Html.text "hello" ]
+    { title = pageTitle
+    , body = [ content model ]
     }
+
+
+content : Model -> Html Msg
+content model =
+    let
+        rows =
+            [ Element.row [ Element.width Element.fill, Element.spacing 20 ] [ titleInput model ]
+            , Element.row [ Element.width Element.fill, Element.spacing 20 ] [ whenInput model ]
+            ]
+
+        column =
+            Element.column
+                [ Element.width (Element.maximum 800 Element.fill)
+                , Element.spacing 20
+                , Element.padding 20
+                , Element.centerX
+                , Font.size 14
+                , Background.color (Element.rgb255 245 247 250)
+                ]
+                rows
+    in
+    Element.layout
+        [ Element.width Element.fill
+        , Element.height Element.fill
+        , Background.color (Element.rgb255 245 247 250)
+        ]
+        column
+
+
+titleInput : Model -> Element Msg
+titleInput model =
+    let
+        label =
+            Input.labelAbove [ Font.bold, Font.size 14 ] (Element.text "Title")
+    in
+    Input.text
+        [ borderColor
+        , Font.size 20
+        , Element.spacing 10
+        ]
+        { onChange = SetTitle
+        , text = model.title
+        , placeholder = Nothing
+        , label = label
+        }
+
+
+whenInput : Model -> Element Msg
+whenInput model =
+    let
+        label =
+            Input.labelAbove [ Font.bold, Font.size 14 ] (Element.text "When")
+    in
+    Input.text
+        [ borderColor
+        , Font.size 20
+        , Element.spacing 10
+        ]
+        { onChange = SetWhen
+        , text = model.when
+        , placeholder = Nothing
+        , label = label
+        }
+
+
+borderColor : Element.Attribute Msg
+borderColor =
+    Border.color (Element.rgb255 211 214 219)
