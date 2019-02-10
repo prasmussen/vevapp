51838ca855defbee7dcb79d5a4fe31334de91d3c
Petter Rasmussen <petter.rasmussen@gmail.com>
2019-02-10 18:07:40 +0100

    [reminders-app] Cleanup init.js

--- reminders-app/init.js
+++ reminders-app/init.js
@@ -1,108 +1,136 @@
-// TODO: handle err
-function init(auth, err) {
-    var maybeUser = maybeUserFromAuth(auth);
+(function() {
 
-    console.log("user", maybeUser);
-    console.log("err", err);
-
-
-    var app = Elm.Main.init({
-        node: document.getElementById("app"),
-        flags: maybeUser,
-    });
-
-    var model = {
-        app: app,
-        auth: auth,
+    var strings = {
+        AuthChange: "AuthChange",
+        SignIn: "SignIn",
+        SignOut: "SignOut",
+        ListReminders: "ListReminders",
+        ListRemindersSuccess: "ListRemindersSuccess",
+        ListRemindersFailure: "ListRemindersFailure",
+        CreateReminder: "CreateReminder",
+        CreateReminderSuccess: "CreateReminderSuccess",
+        CreateReminderFailure: "CreateReminderFailure",
     };
 
-    // Listen for messages from elm
-    app.ports.toJavascript.subscribe(function(msg) {
-        update(msg, model);
-    });
+    function init(auth, err) {
+        var maybeUser = maybeUserFromAuth(auth);
+        var maybeError = formatError(err);
 
-    // Listen for sign-in state changes
-    auth.isSignedIn.listen(function() {
-        update(toMsg("AuthChange"), model);
-    });
-}
+        var app = Elm.Main.init({
+            node: document.getElementById("app"),
+            flags: {
+                user: maybeUser,
+                error: maybeError,
+            }
+        });
 
-function update(msg, model) {
-    switch (msg.tag) {
-        case 'AuthChange':
-            var maybeUser = maybeUserFromAuth(model.auth);
-            model.app.ports.fromJavascript.send(toMsg("AuthChange", maybeUser));
-            break;
+        var model = {
+            app: app,
+            auth: auth,
+        };
 
-        case 'SignIn':
-            model.auth.signIn();
-            break;
+        // Listen for messages from elm
+        app.ports.toJavascript.subscribe(function(msg) {
+            update(msg, model);
+        });
 
-        case 'SignOut':
-            model.auth.signOut();
-            break;
-
-        case 'ListReminders':
-            var options = msg.data;
-
-            gapi.client.calendar.events.list(options).then(function(res) {
-                model.app.ports.fromJavascript.send(toMsg("ListRemindersSuccess", res.result));
-            }, function(res) {
-                model.app.ports.fromJavascript.send(toMsg("ListRemindersFailure", res.result.error.message));
+        if (auth) {
+            // Listen for sign-in state changes
+            auth.isSignedIn.listen(function() {
+                var msg = toMsg(strings.AuthChange, model);
+                update(msg, model);
             });
-            break;
-
-        case 'CreateReminder':
-            var options = msg.data;
-
-            gapi.client.calendar.events.insert(options).then(function(res) {
-                model.app.ports.fromJavascript.send(toMsg("CreateReminderSuccess", res.result));
-            }, function(res) {
-                model.app.ports.fromJavascript.send(toMsg("CreateReminderFailure", res.result.error.message));
-            });
-            break;
-
-        default:
-            console.warn("Unhandled message from elm: " + msg.tag);
-    }
-}
-
-
-function toMsg(tag, data) {
-    return {
-        tag: tag,
-        data: data,
-    };
-}
-
-function maybeUserFromAuth(auth) {
-    if (!auth) {
-        return null;
+        }
     }
 
-    var isAuthorized = auth.isSignedIn.get();
-    if (!isAuthorized) {
-        return null;
+    function update(msg, model) {
+        switch (msg.tag) {
+            case strings.AuthChange:
+                var maybeUser = maybeUserFromAuth(model.auth);
+                sendMsg(model, strings.AuthChange, maybeUser);
+                break;
+
+            case strings.SignIn:
+                model.auth.signIn();
+                break;
+
+            case strings.SignOut:
+                model.auth.signOut();
+                break;
+
+            case strings.ListReminders:
+                var options = msg.data;
+
+                gapi.client.calendar.events.list(options).then(function(res) {
+                    sendMsg(model, strings.ListRemindersSuccess, res.result);
+                }, function(res) {
+                    sendMsg(model, strings.ListRemindersFailure, res.result.error.message);
+                });
+                break;
+
+            case strings.CreateReminder:
+                var options = msg.data;
+
+                gapi.client.calendar.events.insert(options).then(function(res) {
+                    sendMsg(model, strings.CreateReminderSuccess, res.result);
+                }, function(res) {
+                    sendMsg(model, strings.CreateReminderFailure, res.result.error.message);
+                });
+                break;
+
+            default:
+                console.warn("Unhandled message from elm: " + msg.tag);
+        }
     }
 
-    var user = auth.currentUser.get();
-    var profile = user.getBasicProfile();
 
-    return {
-        email: profile.getEmail(),
-    };
-}
+    function sendMsg(model, tag, data) {
+        var msg = toMsg(tag, data);
+        model.app.ports.fromJavascript.send(msg);
+    }
 
-gapi.load('client:auth2', function() {
-    gapi.client.init({
-        'apiKey': 'AIzaSyAYk1JO0wu3UxAbYmeDxLclOA2psEwLQZE',
-        'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
-        'clientId': '853474051422-rbvibfhir17apk19nq9vf3j8sshdo81f.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/calendar',
-    }).then(function() {
-        var auth = gapi.auth2.getAuthInstance();
-        init(auth, null);
-    }, function(err) {
-        init(null, err)
+    function toMsg(tag, data) {
+        return {
+            tag: tag,
+            data: data,
+        };
+    }
+
+    function maybeUserFromAuth(auth) {
+        if (!auth) {
+            return null;
+        }
+
+        var isAuthorized = auth.isSignedIn.get();
+        if (!isAuthorized) {
+            return null;
+        }
+
+        var user = auth.currentUser.get();
+        var profile = user.getBasicProfile();
+
+        return {
+            email: profile.getEmail(),
+        };
+    }
+
+    function formatError(err) {
+        //err.errors
+        return "TODO";
+    }
+
+    gapi.load('client:auth2', function() {
+        gapi.client.init({
+            'apiKey': 'AIzaSyAYk1JO0wu3UxAbYmeDxLclOA2psEwLQZE',
+            'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+            'clientId': '853474051422-rbvibfhir17apk19nq9vf3j8sshdo81f.apps.googleusercontent.com',
+            'scope': 'https://www.googleapis.com/auth/calendar',
+        }).then(function() {
+            var auth = gapi.auth2.getAuthInstance();
+            init(auth, null);
+        }, function(err) {
+            init(null, err)
+        });
     });
-});
+
+})();
